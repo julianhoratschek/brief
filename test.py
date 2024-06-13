2b8d1ee35573baf8dfe9d18255ab945d0bce0930
Julian Horatschek <lando.horaz@gmail.com>
2024-06-14 01:18:41 +0200

    first

---
+++ test.py
@@ -0,0 +1,18 @@
+from zipfile import ZipFile
+import re
+
+
+def extract_text(pre_match: str) -> str:
+    pattern = re.compile(r"<w:t(?:\s.*?)?>(.*?)</w:t>")
+    return "".join([text.group(1) for text in pattern.finditer(pre_match)])
+
+
+def show_all_fields():
+    with ZipFile(r"D:\projects\python\leterip\Bali, Korinna 29032024.docx") as zip_file:
+        with zip_file.open("word/document.xml") as docx_file:
+            pattern = re.compile(r"<w:tc>.*?<w:p(?:\s.*?)?>(.*?)</w:p>")
+            for i, m in enumerate(pattern.finditer(docx_file.read().decode("utf-8"))):
+                print(f"{i}: {extract_text(m.group(1))}")
+
+
+show_all_fields()
